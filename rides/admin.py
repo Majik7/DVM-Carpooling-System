7bf7d575514955f36941871f3208d2ca4022227d
unknown <majikman552@gmail.com>
2026-03-26 17:57:38 +0530

    updated admin page

--- rides/admin.py
+++ rides/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Trip
+from .models import Trip, Offer, CarpoolRequest
 
 # Register your models here.
 admin.site.register(Trip)
+admin.site.register(Offer)
+admin.site.register(CarpoolRequest)
