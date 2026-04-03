87f94db15ce2ffceee835ca222b0e092852b9dd9
unknown <majikman552@gmail.com>
2026-04-03 19:43:01 +0530

    minor change in models

--- network/models.py
+++ network/models.py
@@ -11,5 +11,11 @@ class Edge(models.Model):
     prev_node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name="outgoing", blank=True, null=True)
     next_node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name="incoming", blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.prev_node} - {self.next_node}"
+
 class ServiceStatus(models.Model):
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"active - {int(self.is_active)}"
